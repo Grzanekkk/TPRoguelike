63489f0b58fa66fdf9bc38e3fedc9c62aaabc96a
Grzanekkk <tofukles@gmail.com>
2022-02-08 23:23:30 +0100

    Basic TP movement

---
+++ Source/TPRoguelike/Public/SCharacter.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "SCharacter.generated.h"
+
+UCLASS()
+class TPROGUELIKE_API ASCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ASCharacter();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Camera")
+	class UCameraComponent* CameraComp;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Camera")
+	class USpringArmComponent* SpringArmComp;
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
